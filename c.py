0d58d9c9771c643181ad73e9176d2e8ffb33bd2a
Nick Loadholtes <nick@ironboundsoftware.com>
2012-03-16 17:48:19 -0400

    Adding in two experiments

---
+++ c.py
@@ -0,0 +1,11 @@
+#
+# c.py
+#
+# Nick Loadholtes <nick@ironboundsoftware.com>
+# An Experiment in clustering tweets
+#
+
+
+
+if __name__ == "__main__":
+    print("Clustering experiment")
---
+++ sentiment.py
@@ -0,0 +1,11 @@
+#
+# sentiment.py
+#
+# Nick Loadholtes <nick@ironboundsoftware.com>
+# An experiment in getting the sentiment of tweets
+#
+
+
+
+if __name__ == "__main__":
+    print("Sentiment experiment")
