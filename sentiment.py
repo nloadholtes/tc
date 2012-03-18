737b8d7dd5b659672ad1222a23cf6f3d6516944e
Nick Loadholtes <nick@ironboundsoftware.com>
2012-03-18 14:33:37 -0400

    Starting to the sentiments

--- sentiment.py
+++ sentiment.py
@@ -5,6 +5,16 @@
 # An experiment in getting the sentiment of tweets
 #
 
+SENTIMENTS = enumerate((" Very Positive", "Positive", "Neutral", "Negative", "Very Negative"))
+
+def getSentiment(tweet):
+    """
+    Look at the tweet, and figure out what it is
+
+    Arguments:
+    - `tweet`:
+    """
+    pass
 
 
 if __name__ == "__main__":
