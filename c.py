3cc0d11dd7950474187d12f094e32a530c4a739a
Nick Loadholtes <nick@ironboundsoftware.com>
2012-03-18 14:33:05 -0400

    Fleshing out the clustering

--- c.py
+++ c.py
@@ -5,6 +5,15 @@
 # An Experiment in clustering tweets
 #
 
+def clusterOnTweets(tweets):
+    """Take the tweets, create clusters of similar twees"""
+    words = []
+    clusters = []
+    for tweet in tweets:
+        words.append()
+
+    #Loops through the words, bringing together similar scores
+    return clusters
 
 
 if __name__ == "__main__":
