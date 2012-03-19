0891ae98da2bda3b2d36c647cae7df7f753b5eca
Nick Loadholtes <nick@ironboundsoftware.com>
2012-03-18 22:15:04 -0400

    Adding refernces to the setings file

--- lib/mongodb.py
+++ lib/mongodb.py
@@ -1,7 +1,8 @@
 import logging
 from pymongo import *
+import settings
 
-def connect(dbname, host='localhost', port=27017):
+def connect(dbname, host=settings.MONGO_ADDRESS, port=int(settings.MONGO_PORT)):
     logging.debug("connecting to magicbus mongodb")
     conn = connection.Connection(host, port)
     db = database.Database(conn, dbname)
