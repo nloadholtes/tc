2585d494fa61ab288dc5f2bd3cb8b7dfa230eed6
Nick Loadholtes <nick@ironboundsoftware.com>
2012-03-22 22:00:02 -0400

    Adding in a basic test with tweet generation

--- tests/c_test.py
+++ tests/c_test.py
@@ -1,2 +1,20 @@
 #
-# tests go hear
+# tests go here
+
+import unittest
+import c
+
+def genTweet(intext):
+    class S():
+        text = intext
+    return S()
+
+class ClusterTesting(unittest.TestCase):
+    def setUp(self):
+        self.tweets =[genTweet('first tweet'), genTweet('second tweet'),
+                      genTweet('third tweet')]
+
+    def testClusteronTweets(self):
+        clusters = c.clusterOnTweets(self.tweets)
+        self.assertTrue(clusters is not None)
+
