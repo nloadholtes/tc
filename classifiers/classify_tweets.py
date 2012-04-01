47ecb5d31cd889b1c99ba6836806c807841d0ac5
Nick Loadholtes <nick@ironboundsoftware.com>
2012-04-01 16:43:11 -0400

    M-x whitespace-cleanup

--- classifiers/classify_tweets.py
+++ classifiers/classify_tweets.py
@@ -20,7 +20,7 @@ class tweetClassifier(object):
             for line in f.readlines():
                 data.append((line, category))
         return data # returns a list of tuples of the form (line of text, category)
-        
+
     def document_features(self, document):
         """
         document_features: this breaks a text document into the comparison set of features
@@ -39,24 +39,24 @@ class tweetClassifier(object):
         line_prob = classifier.prob_classify(self.document_features(line)) # get the probability for each category
         for cat in line_prob.samples():
             print "prob. %s = %s" % (cat, line_prob.prob(cat))
-        
-        
+
+
 
 class sports(tweetClassifier):
     SAMPLE_SIZE = 500
     CLASSIFIER_DUMP = 'classifiers/trained/sports'
-    
+
     def __init__(self, debug=False):
         self.debug = debug
-        
+
         try:
             (self.classifier, self.word_features) = pickle.load(open(self.CLASSIFIER_DUMP, 'r'))
         except IOError:
             data = self.load_data(['sports', 'new_york'])
             self.train_classifier(data)
             pickle.dump((self.classifier, self.word_features), open(self.CLASSIFIER_DUMP, 'w'))
-                    
-        
+
+
     def train_classifier(self, data):
         data = random.sample(data, self.SAMPLE_SIZE)
 
@@ -77,38 +77,38 @@ class sports(tweetClassifier):
             print "Accuracy of the classifier: %s" % nltk.classify.accuracy(self.classifier, test_set) # print the accuracy of the classifier
 
         # classifier.show_most_informative_features(100) # show the features that have high relevance
-    
-        
+
+
     def classify(self, tweet):
         line_prob = self.classifier.prob_classify(self.document_features(tweet)) # get the probability for each category
         return ('sports', line_prob.prob('sports'))
 
-        
+
 class narcissism(tweetClassifier):
     N_THRESHOLD = 5.0
-    
+
     def __init__(self):
         self.keywords = ['my latest', 'out my', 'my new', 'i am', 'i hate', 'i love', 'i like', "i can't", 'new post', 'did i', 'i shall', 'i really', 'i wish', 'mine', "i'll", 'i do', "i don't", "i won't", "for my", "i did", "i have", "i had", 'fml']
-        
+
     def classify(self, tweet):
         count = 0
         for word in self.keywords:
             if word in tweet.lower():
                 count += 1
-                
+
         score = min((float(count) / self.N_THRESHOLD), 1.0) # max score = 1.0
         return ('narcissism', score)
-        
+
 class betaworks(tweetClassifier):
     def __init__(self):
         self.keywords = ['tweetdeck', 'chartbeat', 'socialflow', '@bitly', 'venmo', 'tumblr', 'superfeedr', 'backupify', 'fluiddb', 'twitterfeed']
-        
+
     def classify(self, tweet):
         score = 0.0
         for word in self.keywords:
             if word in tweet.lower():
                 score = 1.0
-                
+
         return ('betaworks', score)
 
 class checkin(tweetClassifier):
