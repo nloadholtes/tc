aee09e5ecf92a7c308a5f70759ff0037862d2ca1
Nick Loadholtes <nick@ironboundsoftware.com>
2012-04-01 16:57:11 -0400

    First test

---
+++ classifiers/tests/test_classify_tweets.py
@@ -0,0 +1,12 @@
+#
+# Tests for the classifiers
+#
+# Nick Loadholtes <nick@ironboundsoftware.com>
+# April 1, 2012
+#
+
+import unittest
+
+class TestEmotion(unittest.TestCase):
+    def testNegativeEmotion(self):
+        self.assertTrue(False)
