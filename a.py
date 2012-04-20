e86ea93e358af7f453a0ec7c8563b2c49c250a0c
Nick Loadholtes <nick@ironboundsoftware.com>
2012-04-20 13:32:43 -0400

    Adding in basic analysis code

--- a.py
+++ a.py
@@ -14,7 +14,7 @@ import datetime
 def scoreTweet(tweet, classifier):
     """Looks at the tweet and gives it a score based on
     the type of classifier we are dealing with."""
-    return classifier.classify(tweet)
+    return classifier.classify(tweet['text'])
 
 def saveScore(tweet, score, collection, lt):
     tweet.score = score
@@ -48,3 +48,27 @@ if __name__ == '__main__':
     tweets = json.loads(data)
     f.close()
     print("Number of tweets:" + str(len(tweets)))
+    emotion = Emotion()
+    avg = 0.0
+    total = len(tweets)
+    bad = 0
+    good = 0
+    neutral = 0
+    n_tweets = []
+    for t in tweets:
+        if ' -- ' in t['text']:
+            continue
+        score = scoreTweet(t, emotion)
+        if score >0:
+            good += 1
+        if score == 0:
+            neutral += 1
+            n_tweets.append(t['text'])
+        if score < 0:
+            bad += 1
+        avg += score
+
+    print("Average: " + str(avg/total))
+    print("G,b,n: %d,%d,%d" %(good, bad, neutral))
+    for x in range(141,155):
+        print(n_tweets[x]+"\n")
